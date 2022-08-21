86cc06539ad8987521151347a45e30ecc5f08053
Dzenis Madzovic <dzenis.madzovic@pinmeto.com>
2022-08-21 14:32:12 +0200

    Added python

---
+++ src/Components/Login/login.js
@@ -0,0 +1,15 @@
+import React from "react";
+
+export function Login() {
+    return (
+        <div>
+            <h1>GoldApp</h1>
+            <form>
+                <input></input>
+                <input></input>
+                <button>Login</button>
+                <button>Register</button>
+            </form>
+        </div>
+    );
+}
